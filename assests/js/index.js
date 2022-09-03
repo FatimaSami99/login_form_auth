8ab6a5e6c1828d0bd759846784f060921e1b43c3
FatimaSami99 <98578690+FatimaSami99@users.noreply.github.com>
2022-09-03 13:19:25 +0500

    Update index.js

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,5 +1,7 @@
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
 import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+
 const auth = getAuth()
 function register(){
 
@@ -10,6 +12,28 @@ function register(){
     
     console.log(username.value);
     createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => {
+            alert("new user is registered!")
+        })
+        .catch((err) => {
+            alert("error")
+            console.log("error is there =", err);
+        })
+
+}
+document.getElementById("register").addEventListener("click", register)
+
+function login() {
+    let password = document.querySelector("#passwordForLogin")
+    let email = document.querySelector("#emailForLogin")
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then(() => {
+            alert("successfully login!")
+        })
+        .catch((err) => {
+            alert("error")
+            console.log("error is there =", err);
+        })
 
 }
 document.getElementById("register").addEventListener("click" , register)
